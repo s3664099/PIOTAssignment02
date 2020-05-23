5191831d5956972cf2920a6a08d0a724136776dd
Bhavi22 <bhavi.smehta@gmail.com>
2020-05-23 17:35:14 +1000

    changing path for token & credentials, to avoid manual file movements

--- Database/gcalendar_utils.py
+++ Database/gcalendar_utils.py
@@ -21,12 +21,12 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 def connect_calendar():
 
     # If modifying these scopes, delete the file token.json.
-    store = file.Storage("../token.json")
+    store = file.Storage("token.json")
     creds = store.get()
 
     if(not creds or creds.invalid):
 
-        flow = client.flow_from_clientsecrets("../credentials.json", SCOPES)
+        flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
         creds = tools.run_flow(flow, store)       
 
     return build("calendar", "v3", http=creds.authorize(Http()))
