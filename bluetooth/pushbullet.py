64b94a94fe549058f4fe6979d4b3360840eadd32
s3664099 <s3664099@student.rmit.edu.au>
2020-06-04 16:47:23 +1000

    Added some comments to pb

--- bluetooth/pushbullet.py
+++ bluetooth/pushbullet.py
@@ -1,10 +1,16 @@
 import requests
 import json
 
+#Method to send a push bullet notification
+#The method takes a title, a message, and the token
+#The token is stored in the database and relates to the engineer assigned the tasl
+#The message comprises of othe details of the vehicle
 def send_notification(title, message, token):
 
+	#Constructs the json file for push bullet
     data_send = {"type": "note", "title": title, "body": message}
- 
+
+	#Calls the pushbullet API to send the message 
     resp = requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(data_send),
                          headers={'Authorization': 'Bearer ' + token, 
                          'Content-Type': 'application/json'})
